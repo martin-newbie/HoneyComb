5ef72de2259d7502ac710eb7b2b12200bd885248
BlackPenguin <dkstmdwnswns@naver.com>
2022-04-11 20:33:35 +0900

    cartoon

--- HoneyComb/Assets/Black_Penguin/CartoonManager.cs
+++ HoneyComb/Assets/Black_Penguin/CartoonManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Threading.Tasks;
 
 public class Cartoon
 {
@@ -12,15 +13,30 @@ public class Cartoon
 
 public class CartoonManager : MonoBehaviour
 {
-    Cartoon[] Cartoons;
-    IEnumerator CartoonStart(Cartoon[] funcCartoons,Action<Image> action)
+    float MaxDuration;
+    IEnumerator CartoonStart(Cartoon[] funcCartoons, Action<Image> action)
     {
-        foreach (Cartoon cartoon in funcCartoons )
+        foreach (Cartoon cartoon in funcCartoons)
         {
             cartoon.image.gameObject.SetActive(true);
-            cartoon
             while (Input.GetMouseButtonDown(0))
                 yield return null;
         }
     }
+    public IEnumerator CartoonShake(RectTransform rectPostition, float shakeScale, int shakeTime, float shakeRange, float duration)
+    {
+        if (MaxDuration < shakeTime)
+            MaxDuration = shakeTime;
+        Vector2 pos = rectPostition.localPosition;
+        while (duration <= 0)
+        {
+            rectPostition.position = UnityEngine.Random.insideUnitCircle * shakeScale * pos;
+
+            yield return new WaitForSeconds(shakeTime);
+            duration -= shakeTime * Time.deltaTime;
+
+        }
+        rectPostition.localPosition = pos;
+    }
 }
+
