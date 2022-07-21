0fbec5fefa0ddff9685257cac9059c6782d13c9f
BlackPenguin <dkstmdwnswns@naver.com>
2022-07-22 08:38:21 +0900

    SoundManager

--- HoneyComb/Assets/Black_Penguin/Scripts/SoundManager.cs
+++ HoneyComb/Assets/Black_Penguin/Scripts/SoundManager.cs
@@ -6,7 +6,8 @@ using UnityEngine.SceneManagement;
 public enum SoundType
 {
     BGM,
-    SE
+    SE,
+    END
 }
 
 public class SoundManager : Singleton<SoundManager>
@@ -17,24 +18,23 @@ public class SoundManager : Singleton<SoundManager>
     private void Awake()
     {
         SceneManager.sceneLoaded += whenSceneLoad;
-        DontDestroyOnLoad(this.gameObject);
-        
+        DontDestroyOnLoad(gameObject);
+
         //리소스 폴더안 Sounds폴더안에 오디오클립들을 모은다
         AudioClip[] clips = Resources.LoadAll<AudioClip>("Sounds/");
         foreach (AudioClip clip in clips)
             audioClips[clip.name] = clip;
 
         //오디오 소스 생성후 사운드매니저에 상속
-        GameObject BgmObj = new GameObject();
-        audioSources[SoundType.BGM] = BgmObj.AddComponent<AudioSource>();
-        BgmObj.transform.parent = transform;
-        audioSources[SoundType.BGM].loop = true;
-        audioVolume[SoundType.BGM] = 0.5f;
+        for (SoundType i = 0; i < SoundType.END; i++)
+        {
+            GameObject SourceObj = new GameObject(i.ToString());
+            audioSources[i] = SourceObj.AddComponent<AudioSource>();
+            audioVolume[i] = 0.5f;
+            SourceObj.transform.parent = transform;
 
-        GameObject SeObj = new GameObject();
-        audioSources[SoundType.SE] = SeObj.AddComponent<AudioSource>();
-        SeObj.transform.parent = transform;
-        audioVolume[SoundType.SE] = 0.5f;
+            if (i == SoundType.BGM) audioSources[i].loop = true;
+        }
     }
     public void PlaySound(string ClipName, SoundType type = SoundType.SE, float Volume = 1, float Pitch = 1)
     {
@@ -42,17 +42,16 @@ public class SoundManager : Singleton<SoundManager>
         {
             case SoundType.BGM:
                 audioSources[type].clip = audioClips[ClipName];
-                audioSources[type].Play();
                 audioSources[type].volume = Volume * audioVolume[type];
-                audioSources[type].pitch = Pitch;
+                audioSources[type].Play();
                 break;
-            case SoundType.SE:
+            default:
                 audioSources[type].PlayOneShot(audioClips[ClipName], Volume * audioVolume[type]);
-                audioSources[type].pitch = Pitch;
                 break;
         }
+        audioSources[type].pitch = Pitch;
     }
-    private void whenSceneLoad(Scene level,LoadSceneMode mode)
+    private void whenSceneLoad(Scene level, LoadSceneMode mode)
     {
         //씬이 불러와질때 어떤씬이냐에 따라 배경음악을 전환한다
         switch (level.name)
