3e47848d7a66a70b631133712a41e50aab20d9e2
BlackPenguin <dkstmdwnswns@naver.com>
2022-08-19 17:40:39 +0900

    Daily Quest

--- HoneyComb/Assets/Black_Penguin/Scripts/DailyQuest.cs
+++ HoneyComb/Assets/Black_Penguin/Scripts/DailyQuest.cs
@@ -1,5 +1,8 @@
+using Newtonsoft.Json;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public enum QuestType
@@ -47,15 +50,47 @@ public abstract class BaseDailyQuest
 }
 public class DailyQuest : MonoBehaviour
 {
+    private string receiveDailyQuestTimeString = "DaillyQuest DataString";
+
     public int distance;
     public int getHoneyCount;
     public int hitCount;
-    void Update()
-    {
+    public int moveCount;
+    public int playCount;
 
-    }
+    public int makingWaxCount;
+    public int makingBeeCount;
     private void Start()
     {
+        CheckTimeToReset();
         List<BaseDailyQuest> baseDailyQuests = new List<BaseDailyQuest>();
+    }
+
+    /// <summary>
+    /// 일일퀘스트는 새벽 4시마다 갱신된다
+    /// </summary>
+    void CheckTimeToReset()
+    {
+        string lastTime = PlayerPrefs.GetString(receiveDailyQuestTimeString, "null");
+        if (lastTime != "null")
+        {
+            string[] nowtime = DateTime.Now.ToString("yyyy-MM-dd-HH").Split('-');
+            string dataString = JsonUtility.ToJson(nowtime);
+            PlayerPrefs.SetString(receiveDailyQuestTimeString, dataString);
+        }
+        else
+        {
+            QuestReset();
+        }
+
+    }
+    void QuestReset()
+    {
+        distance = 0;
+        hitCount = 0;
+        moveCount = 0;
+        playCount = 0;
+        makingWaxCount = 0;
+        makingBeeCount = 0;
     }
 }
